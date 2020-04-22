9ac6c2b2a978be9206c3030ade13c00bbee0decf
Joey Piccola <joey.piccola@gmail.com>
2020-04-22 08:59:09 -0600

    module loads in PowerShellBuild Test-PSBuildPester

--- Stucco/template/tests/Help.tests.ps1
+++ Stucco/template/tests/Help.tests.ps1
@@ -1,15 +1,7 @@
 
 # Taken with love from @juneb_get_help (https://raw.githubusercontent.com/juneb/PesterTDD/master/Module.Help.Tests.ps1)
 
-$outputDir       = Join-Path -Path $ENV:BHProjectPath -ChildPath 'Output'
-$outputModDir    = Join-Path -Path $outputDir -ChildPath $env:BHProjectName
-$manifest        = Import-PowerShellDataFile -Path $env:BHPSModuleManifest
-$outputModVerDir = Join-Path -Path $outputModDir -ChildPath $manifest.ModuleVersion
-
 # Get module commands
-# Remove all versions of the module from the session. Pester can't handle multiple versions.
-#Get-Module $env:BHProjectName | Remove-Module -Force
-Import-Module -Name (Join-Path -Path $outputModVerDir -ChildPath "$($env:BHProjectName).psd1") -Verbose:$false -ErrorAction Stop
 $commands = Get-Command -Module (Get-Module $env:BHProjectName) -CommandType Cmdlet, Function # Not alias
 
 ## When testing help, remember that help is cached at the beginning of each session.
